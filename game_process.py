0693d3a5d4b2bdb3e287745b3cdbec4d4db27970
Venice-pasha <155968373+Venice-pasha@users.noreply.github.com>
2024-02-04 10:18:57 +0100

    Add files via upload

--- game_process.py
+++ game_process.py
@@ -1,6 +1,4 @@
 import threading
-import queue
-import random
 import structure as s 
 import socket
 import copy
@@ -12,14 +10,17 @@ class Game:
         manager=multiprocessing.Manager()
         self.number_of_players = number_of_players
         self.deck = self.create_deck()
-        self.players_hands = manager.list([None] * number_of_players)
+        self.players_hands = manager.list()
+        for _ in range(number_of_players):
+            player_hand = [self.deck.draw_card() for _ in range(5)]
+            self.players_hands.append(player_hand)
         self.lock = multiprocessing.Lock()
         self.information_tokens= multiprocessing.Value('i', number_of_players+3)
         self.fuse_tokens = multiprocessing.Value('i', 3)
         self.victory_pool = manager.dict(self.create_victory_pool())
         self.init_barrier = multiprocessing.Barrier(number_of_players + 1)
-        self.to_main_queue = multiprocessing.Queue()  # 子线程向主线程发送消息的队列
-        self.to_child_queues = [multiprocessing.Queue() for _ in range(number_of_players)]  # 主线程向子线程发送消息的队列
+        self.to_main_queue = multiprocessing.Queue()
+        self.to_child_queues = [multiprocessing.Queue() for _ in range(number_of_players)] 
         self.turn_condition = multiprocessing.Condition(self.lock)
         self.game_over = multiprocessing.Event()
         self.current_turn = multiprocessing.Value('i',0)
@@ -29,28 +30,45 @@ class Game:
         self.turn_start = [multiprocessing.Event() for _ in range(number_of_players)]
         self.turn_end = [multiprocessing.Event() for _ in range(number_of_players)]
         self.server_main('127.0.0.1', 65432)
+        print(self.victory_pool)
+        total_cards = sum(self.victory_pool.values())
+        total_score = total_cards
+        print(f"Total score: {total_score}\n")
+        print("end game!")
 
 
+    '''
+    manager: Used to create shared variables that are safe in a multiprocessing context.
+    number_of_players: The number of players in the game.
+    deck: A pile of cards containing all the cards needed for the game.
+    players_hands: A shared list storing the hand of cards for each player.
+    lock: A multiprocessing lock used for synchronizing access to shared resources.
+    information_tokens: Stores the number of information tokens in the game.
+    fuse_tokens: Stores the number of fuse tokens in the game.
+    victory_pool: Records the highest number for each color of card.
+    init_barrier: A barrier to ensure all processes are ready before starting the game.
+    to_main_queue: A queue for child processes to send messages to the main process.
+    to_child_queues: An array of queues for each player, used for the main process to send messages to child processes.
+    game_over: An event flag to indicate whether the game has ended.
+    current_turn: The number of the player whose turn it currently is.
+    played_cards: Stores cards that have been played or discarded.
+    listen_process: A listening thread for handling messages sent from child processes to the main process.
+    turn_start and turn_end: Arrays of event flags to control the start and end of each player's turn.
+    '''
 
+    #Sets up the server, waits for players to connect, and initiates game logic.
     def server_main(self,server_ip, server_port):
-    # 存储客户端连接
         client_connections = []
-
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((server_ip, server_port))
             s.listen()
-
             print(f"Server listening on {server_ip}:{server_port}. Waiting for {self.number_of_players} players to connect.")
-
-            # 等待直到所有玩家连接
             while len(client_connections) < self.number_of_players:
                 conn, addr = s.accept()
                 print(f"Player connected from {addr}")
                 client_connections.append(conn)
             self.client_connections=client_connections
-            # 所有玩家都已连接，开始主游戏逻辑
             print("All players connected. Starting game.")
-                    # 创建和启动玩家线程
             self.player_processes = []
             for i in range(self.number_of_players):
                 process = multiprocessing.Process(target=self.player_action, args=(i,))
@@ -58,21 +76,20 @@ class Game:
                 process.start()
             self.init_barrier.wait()
             time.sleep(3)
-            #index=0事件开始
             while not self.game_over.is_set():
                 with self.lock:
                     current_player = self.current_turn.value
-                self.turn_start[current_player].set()  # 通知当前玩家开始回合
+                self.turn_start[current_player].set()  
                 self.turn_end[current_player].wait()
+                if self.game_over.is_set():
+                    break
                 self.turn_end[current_player].clear()
                 with self.lock:
                     self.current_turn.value = (self.current_turn.value + 1) % self.number_of_players
-            # self.game_barrier.wait()
-            # 等待所有线程完成
             for process in self.player_processes:
                 process.join()
 
-
+    # Broadcasts a message to all clients.
     def broadcast(self, message):
         encoded_message = f"Broadcast: {message}".encode()
         for conn in self.client_connections:
@@ -81,6 +98,7 @@ class Game:
             except socket.error as e:
                 print(f"Error sending message to client: {e}")
     
+    # Sends information to a specified client.
     def unicast_info(self,message,index):
         if index < len(self.client_connections):
             encoded_message = f"Info: {message}".encode()
@@ -91,10 +109,10 @@ class Game:
         else:
             print(f"Invalid client index: {index}")
 
-
+    # Requests and retrieves input from a specified client.
     def unicast_input(self,message,index):
         if index < len(self.client_connections):
-            encoded_message = f"Input: {message}".encode()
+            encoded_message = (f"Input: {message}").encode()
             response_received=False
             try:
                 conn = self.client_connections[index]
@@ -113,13 +131,13 @@ class Game:
         else:
             print(f"Invalid client index: {index}")
 
-
+    # Closes all client connections.
     def close_all_connections(self):
-        """关闭所有客户端连接"""
         for conn in self.client_connections:
             conn.close()
         self.client_connections = []
 
+    #  Listens for messages sent from child processes to the main process.
     def listen_to_son(self):
         while not self.game_over.is_set():
             while not self.to_main_queue.empty():
@@ -127,9 +145,7 @@ class Game:
                 print("Main thread received:", message)
                 if message.startswith("Information"):
                     parts = message.split(":")
-                    player_dest = int(parts[1].split()[1])  # 假设消息格式正确
-
-                    # 修改消息格式并发送给相应的玩家
+                    player_dest = int(parts[1].split()[1]) 
                     new_message = f"to player {player_dest}: {message}"
                     self.to_child_queues[player_dest].put(new_message)
                 elif message.startswith("Play"):
@@ -138,12 +154,10 @@ class Game:
                 elif message.startswith("Discard"):
                     for i in range(self.number_of_players):
                         self.to_child_queues[i].put(message)
-                elif message.startwith("choose action:"):
-                    pass
                 else:
-                    # 处理其他类型的消息
                     pass
 
+    # Creates and initializes the deck of cards.
     def create_deck(self):
         if self.number_of_players == 2:
             self.colors = ['Red', 'Bleu'] 
@@ -153,9 +167,11 @@ class Game:
         deck = s.Deck(self.colors, numbers) 
         return deck
     
+    # Initializes the victory pool.
     def create_victory_pool(self):
         return {color: 0 for color in self.colors}
     
+    # Shows other players' hands to a specified player.
     def show_sight(self,player_index):
         with self.lock:
             message=(f"show {player_index}'s sight\n")
@@ -168,6 +184,7 @@ class Game:
                     message=(f"You are {index} you cannot see your card\n")
                     self.unicast_info(message,player_index)
 
+    # Prints the state of the victory pool to a specified player.
     def print_vpool(self,player_index):
         with self.lock:
             message=("Victory Pool:")
@@ -180,37 +197,42 @@ class Game:
             message=(f"Total score: {total_score}\n")
             self.unicast_info(message,player_index)
 
+    # Checks if the game is over and processes accordingly.
     def is_game_over(self):
         # case1: victory
         all_fireworks_completed = all(value == 5 for value in self.victory_pool.values())
         if all_fireworks_completed:
-            message=("Victory! All fireworks are completed.\n")
-            self.broadcast(message)
-            self.game_over=True
+            message = "Game over: Victory! All fireworks are completed.\n"
         # case2: fuse token all used
-        if self.fuse_tokens.value == 0:
-            message=("Game over! All fuse tokens are used.\n")
-            self.broadcast(message)
-            self.game_over=True
+        elif self.fuse_tokens.value == 0:
+            message = "Game over: All fuse tokens are used.\n"
         # case3: deck all used
-        if not self.deck:
-            message=("Game over! No more cards in the deck.\n")
-            self.broadcast(message)
-            self.game_over=True
-        message=("Game continues.\n")
-        self.broadcast(message)
+        elif not self.deck:
+            message = "Game over: No more cards in the deck.\n"
+        else:
+            message = "Game continues.\n"
 
+        # Broadcasting the message to all clients
+        if "Game over" in message:
+            for i in range(self.number_of_players):
+                self.print_vpool(i)
+                self.broadcast(message)
+                self.game_over.set()  
+                self.close_all_connections() 
+                for event in self.turn_start:
+                    event.set()
+
+    # Handles the action chosen by the player (inform, play card, or discard).
     def card_action(self, player_index,player_hand):
         action=self.input_action(player_index)
-        action_success=False
-        while not action_success:
-            if action == 'inform':
-                action_success=self.inform(player_index)
-            elif action == 'play':
-                action_success=self.play_card(player_index,player_hand)
-            elif action == 'discard':
-                action_success=self.discard_card(player_index,player_hand)
-
+        if action == 'inform':
+            self.inform(player_index)
+        elif action == 'play':
+            self.play_card(player_index,player_hand)
+        elif action == 'discard':
+            self.discard_card(player_index)
+        
+    # Requests and gets the player's choice of action.
     def input_action(self,player_index):
         input_sucess=False
         while not input_sucess:
@@ -232,8 +254,10 @@ class Game:
             else:
                message=("wrong index!Try again.\n")
                self.unicast_info(message,player_index)
+            time.sleep(0.5)
         return action
 
+    # Processes the inform action.
     def inform(self,player_index):
         # input and verify the info dest
         input_sucess=False
@@ -279,10 +303,9 @@ class Game:
             qty,place=self.info_cardnumber(player_hand,action_info)
             message=(f"Information from {player_index}: Player {player_dest} has {action_info} card, quantity{qty}, place{place}.\n")
         self.to_main_queue.put(message)
-            #把这条信息发送信息给主程序
         return True
             
-    #level 4 programme, subject of inform
+    # Provide information about specific cards.
     def info_cardnumber(self,player_hand,n):
         count = 0
         positions = []
@@ -293,8 +316,6 @@ class Game:
                 count += 1
                 positions.append(index)
         return count, positions
-
-    #level 4 programme, subject of inform
     def info_cardcolor(self,player_hand,c):
         count = 0
         positions = []
@@ -304,10 +325,12 @@ class Game:
                 positions.append(index)
         return count, positions
 
+    # Processes the play card action.
     def play_card(self, player_index, player_hand):
+        #ask for input
         input_success=False
         while not input_success:
-            message="which card do you want to play, choose from 0 to 4:"
+            message="Input:which card do you want to play, choose from 0 to 4:\n:"
             card_index=self.unicast_input(message,player_index)
             if card_index.isdigit() and int(card_index) in [1, 2, 3, 4, 0]:
                 card_index = int(card_index)
@@ -315,17 +338,10 @@ class Game:
             else:
                 message=("illegal info!\n")
                 self.unicast_info(message,player_index)
+        #use card
         card_played=copy.deepcopy(player_hand[card_index])
         color=card_played.color
         number=card_played.number
-        '''with self.lock:
-            if self.victory_pool[color] == number-1:
-                self.victory_pool[color] = number
-                message=(f"Play:you added sucessfully{card_index} with {color} and {number} to victory pool!\n")
-            else:
-                self.fuse_tokens.count -=1
-                self.played_cards.append(card_played)
-                message=(f"Play:you droped your {card_index} with {color} and {number} and lose a fuse_token!\n")'''
         if self.victory_pool[color] == number-1:
             self.victory_pool[color] = number
             message=(f"Play:Player{player_index} added sucessfully card{card_index} with {color} and {number} to victory pool!\n")
@@ -334,70 +350,74 @@ class Game:
                 self.fuse_tokens.value -=1
             self.played_cards.append(card_played)
             message=(f"Play:Player{player_index} droped card{card_index} with {color} and {number} and lose a fuse_token!\n")
-        
         self.to_main_queue.put(message)
-        '''with self.lock:
-            player_hand[player_index]=self.deck.draw_card()
-            self.players_hands[player_index]=player_hand'''
-        player_hand[player_index]=self.deck.draw_card()
         with self.lock:
-            self.players_hands[player_index]=player_hand
-        print("a new card is in your hand, replace the card you've used.\n ")
+            new_hand = list(self.players_hands[player_index])  
+            new_hand[card_index] = self.deck.draw_card() 
+            self.players_hands[player_index] = new_hand
+
+        message=("a new card is in your hand, replace the card you've used.\n ")
+        self.unicast_info(message,player_index)
         return True
     
-    #level 3 programme, 3rd kind
+    #Processes the discard card action.
     def discard_card(self, player_index):
         input_success=False
         while not input_success:
-            card_index=input("which card do you want to throw away, choose from 0 to 4:")
+            message=("which card do you want to throw away, choose from 0 to 4:\n")
+            card_index=self.unicast_input(message,player_index)
             if card_index.isdigit() and int(card_index) in [1, 2, 3, 4, 0]:
                 card_index = int(card_index)
                 input_success = True
             else:
-                print("illegal info!")
+                message="illegal info!\n"
+                self.unicast_info(message,player_index)
         player_hand=self.players_hands[player_index]
-        discard=copy.deepcopy(player_hand.cards[card_index])
+        discard=copy.deepcopy(player_hand[card_index])
         self.played_cards.append(discard)
-        (self.players_hands[player_index].cards)[card_index]=self.deck.draw_card()
-        print("a new card is in your hand, replace the card you've used.\n ")
-        '''with self.lock:
-            self.information_tokens += 1'''
-        self.information_tokens += 1
-        message=("Discard:We've got a new info token. Vivez la nouvelle regle!")
+        with self.lock:
+            (self.players_hands[player_index])[card_index]=self.deck.draw_card()
+        message=("a new card is in your hand, replace the card you've used.\n ")
+        self.unicast_info(message,player_index)
+        with self.lock:
+            self.information_tokens.value += 1
+        message=("Discard:We've got a new info token. Vivez la nouvelle regle!\n")
         self.to_main_queue.put(message)
         return True
 
+    # Controls the flow of a player's actions.
     def player_action(self, player_index):
+        #initialisation
         listen_thread = threading.Thread(target=self.listen_to_main, args=(player_index,))
         listen_thread.start()
-        player_hand=[]
-        for _ in range(5): 
-            '''with self.lock:'''
-            card = self.deck.draw_card()
-            if card:
-                player_hand.append(card)
-        with self.lock:
-            self.players_hands[player_index]=player_hand
-        self.players_hands[player_index]=player_hand
         self.init_barrier.wait()
-
+        #loop to break game
         while not self.game_over.is_set(): 
+
             self.turn_start[player_index].wait()
-            self.turn_start[player_index].clear()
             if self.game_over.is_set():
+                listen_thread.join()
                 break
+            self.turn_start[player_index].clear()
             message=(f"{player_index}'s turn!\n")
+
             self.broadcast(message)
+            player_hand=self.players_hands[player_index]
             self.show_sight(player_index)
             self.print_vpool(player_index)
             self.card_action(player_index,player_hand)
             self.is_game_over()
+            # help to clean game
+            if self.game_over.is_set():
+                self.turn_start[(player_index+1)%self.number_of_players].is_set()
+                self.turn_end[player_index].set()
+                listen_thread.join()
+                break
             message=(f"player {player_index} finished")
             self.broadcast(message)
             self.turn_end[player_index].set()
-        #self.game_barrier.wait()
-        listen_thread.join()
-
+        
+    # Listens and processes messages coming from the main process.
     def listen_to_main(self, player_index):
         while not self.game_over.is_set():
             if not self.to_child_queues[player_index].empty():
@@ -407,8 +427,4 @@ class Game:
                 except socket.error as e:
                     print(f"Error sending message to player {player_index}: {e}")
 
-
-            
-
-# 运行游戏
 game = Game(number_of_players=2)
--- game_process_client.py
+++ game_process_client.py
@@ -1,8 +1,9 @@
 import socket
+import time
 
 def client_main(server_ip, server_port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((server_ip, server_port))
+        s.connect((server_ip, server_port)) # Establish connection with server
         print(f"Connected to server at {server_ip}:{server_port}")
 
         buffer=""
@@ -12,39 +13,23 @@ def client_main(server_ip, server_port):
                 break
             buffer += data.decode()
 
-            # 处理所有完整的消息
+            # Process complete messages separated by newline
             while '\n' in buffer:
                 message, buffer = buffer.split('\n', 1)
-                if message=="Game Over":
+                if message.startswith("Broadcast: Game over"):
                     print("Game Over")
+                    time.sleep(2)
                     return
-                process_message(s, message)
-        
-def receive_messages(sock, message_received_event):
-    while True:
-        try:
-            data = sock.recv(1024).decode()
-            if not data or data.lower() == 'quit':
-                break
-            print(f"Received from server: {data}")
-            message_received_event.set()  # Signal that a message has been received
-        except Exception as e:
-            print(f"Error receiving data: {e}")
-            break
-
-
+                else:
+                    process_message(s, message)
 
 def process_message(sock, message):
-    if message.startswith("Broadcast: "):
-        print(message[len("Broadcast: "):])
-    elif message.startswith("Info: "):
-        print(message[len("Info: "):])
-    elif message.startswith("Input: "):
+    if message.startswith("Input: "):
         user_input = input(message[len("Input: "):])
-        sock.sendall(user_input.encode() + b'\n')  # 发送回应给服务器
+        sock.sendall(user_input.encode() + b'\n')  
+        # if input, Send response back to server
     else:
         print(message)
+        # or just print it
 
-
-# 使用示例
 client_main('127.0.0.1', 65432)
